3687a65f0d70ffe295777fa4d3347ba0a52633f8
Jenia <bevswf@gmail.com>
2022-03-26 19:08:51 +0300

    version 0.5

---
+++ index.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+
+<html>
+    <head>  
+        <title>Company</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet"  href="css/main.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    </head>
+
+    <body>
+
+        <!-- navbar section -->
+
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand"><img class="logo" src="img/test/logo.png"> </a>
+
+                </div>
+
+
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a class="trs" href="#">HOME</a></li>
+                        <li><a href="#">ABOUT US</a></li>
+                        <li><a href="#">OUR FOCUS</a></li>
+                        <li><a href="#">PORTFOLIO</a></li>
+                        <li><a href="#">OUR TEAM</a></li>
+                        <li><a href="#">PRICING</a></li>
+                        <li><a href="#">CONTACTS</a></li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <!-- header section -->
+      
+
+        <div id="headerwrap">
+            <div class="container">
+                <div class="row centered">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        
+                        <h1>We making thing with love </br>and passion</h1>
+                   
+                   <input class="headerwrapBtn" type="button" value="Our Works">
+                
+
+                </div>
+              
+                  
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- about section -->
+
+    <div id="aboutwrap">
+        <div class="container">
+            <div class="row centered">
+                    
+                    <div class="col-lg-6 col-lg-offset-5 ">
+                            <div class="aboutImgBg"></div>
+                            <div class="aboutTable">
+                                <h1 class="aboutCaption">About Us</h1>
+                                <p class="aboutTextStyling">Analytics release series A financing launch party interaction 
+                                    design android angel investor. Vesting period social media sales
+                                     conversion launch party rockstar incubator customer social 
+                                     proof graphical user interface technology supply chain. 
+                                     Non-disclosure agreement rockstar creative partner network launch 
+                                     party social proof paradigm shift long tail influencer. 
+                                    </p>
+
+                                      <p class="aboutTextStyling">  Monetization validation success. Seed round channels product 
+                                        management. Customer non-disclosure agreement strategy 
+                                        android growth hacking ramen buyer.
+                                    </p>
+                                <div class="lineWraper">
+                                    <div class="lineOne"></div>
+                                    <div class="lineTwo"></div>
+                                    <div class="lineThree"></div>
+                                    <div class="lineFour"></div>
+                                </div>
+                            </div>
+        
+                </div>  
+        </div>
+    </div>
+</div>
+
+    <!-- company Target section -->
+
+    <div class="targetWrap">
+        <div class="container">
+            <div class="row centered">
+                    <h1 class="targetCaption">Our Focus</h1>
+                    <p class="targetTextStyling">We understand your requirement and provide quality works.</p>
+
+                    <div class="targetColumnsWrap ">
+                    <div class="col-lg-3 col-md-6 ">
+                        
+                        <div class="targetColumnOne">
+                            <img class="targetColumnsIcons" src="img/targetColumnOne.png" style="height: 45px">
+                            <p class="targetColumnsHeaders">Web Design</p>
+                            <p class="targetColumnsText">Analytics release series A financing launch party 
+                                    interaction design android angel investor.</p>
+                                    <div class="targetColumnLine" style="background-color: #fc5f45"></div>
+                        </div>
+                    </div>
+                        <div class="col-lg-3 col-md-6 ">
+                        <div class="targetColumnTwo">
+                            <img class="targetColumnsIcons" src="img/targetColumnTwo.png" style="height: 45px">
+                            <p class="targetColumnsHeaders">Ux Design</p>
+                            <p class="targetColumnsText">Analytics release series A financing launch party 
+                                    interaction design android angel investor.</p>
+                                    <div class="targetColumnLine" style="background-color: #38c695"></div>
+                        </div>
+                    </div>
+                        <div class="col-lg-3 col-md-6">
+                        <div class="targetColumnThree" >
+                            <img  class="targetColumnsIcons" src="img/targetColumnThree.png" style="height: 45px">
+                            <p class="targetColumnsHeaders">Photogrpahy</p>
+                            <p class="targetColumnsText">Analytics release series A financing launch party 
+                                    interaction design android angel investor.</p>
+                                    <div class="targetColumnLine" style="background-color: #b27cf5"></div></div>
+                        </div>
+                    </div>
+                        <div class="col-lg-3  col-md-6">
+                        <div class="targetColumnFour">
+                            <img class="targetColumnsIcons" src="img/targetColumnFour.png" style="height: 45px">
+                            <p class="targetColumnsHeaders">App Development</p>
+                            <p class="targetColumnsText">Analytics release series A financing launch party 
+                                    interaction design android angel investor.</p>
+                                    <div class="targetColumnLine" style="background-color: #feb960"></div></div>
+                        </div>
+                    </div>
+                </div>
+
+                   
+                 </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- vidio section -->
+
+    <div class="vidioWrap">
+            <iframe class="corporateVidio" width="1920" height="700" src="https://www.youtube.com/embed/VEjlOa4RiSw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            </div>
+
+
+            <!-- portfolio section -->
+
+            <div class="portfolioWrap">
+                    <div class="container">
+                        <div class="row centered">
+                            <h1 class="portfolioCaption">Portfolio</h1>
+                            <p class="portfolioTextStyle">We understand your requirement and provide quality works.</p>
+
+                            <div class="galleryWrap">
+
+                            <div class="galleryOne">
+                                <div><img src="img/gallery/imgTwo.png"></div>
+                                <div><img src="img/gallery/imgThree.png"></div>
+                                <div><img src="img/gallery/imgFour.png"></div>
+                                <div><img src="img/gallery/imgFive.png"></div>
+                            </div>
+
+                            <div class="galleryOne">
+                                    <div><img src="img/gallery/imgSix.png"></div>
+                                    <div><img src="img/gallery/imgSeven.png"></div>
+                                    <div><img src="img/gallery/imgEit.png"></div>
+                                    <div><img src="img/gallery/imgNine.png"></div>
+                                </div>
+
+                                </div>
+
+                
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <!-- reqvest section -->
+
+
+
+                    <div class="reqvestWrap">
+                        <div class="container">
+                            <div class="row centered">
+                                    <h1 class="reqvestCaption">Get in Touch</h1>
+                                    <p class="reqvestTextStyling">We understand your requirement and provide quality works.</p>
+                                    <form class="validationStyle">
+                                            <div class="row">
+                                              <div class="col-lg-4 col-lg-offset-2">
+                                                <input type="text"  class="form-control" placeholder="Name">
+                                                <input type="text"  class="form-control" placeholder="Email">
+                                              </div>
+                                              <div class="col-lg-4">
+                                                <input type="text" class="form-control" placeholder="Phone">
+                                                <input type="text" class="form-control" placeholder="Skype">
+                                              </div>
+                                              <div class="col-lg-8 col-lg-offset-2 ">
+                                                    <textarea class="form-control" placeholder="Your Message" rows="5"  ></textarea>
+                                                    <input  type="button" class="reqvestBtn" value="Send">
+                                                </div>
+                                            </div>
+                                          </form>
+
+
+
+
+                                            <!-- Footer section -->
+
+
+
+                                            <div class="footerBg">
+                                                 <div class="container">
+                                                    <div class="row centered">
+                                                        <div class="col-lg-3">
+                                                                <img class="footerLogo" src="img/test/logo.png">
+                                                                
+                                                          </div>
+                                                             <div class="col-lg-3">
+                                                                <ul>
+                                                                 <li class="companyStyle">COMPANY</li>
+                                                                 <li class="footerNavStyle">Our prices</li>
+                                                                 <li class="footerNavStyle">Our blog</li>
+                                                               </ul>
+                                                             </div>
+                                                                 <div class="col-lg-3">
+                                                                  <ul>
+                                                                   <li class="contactStyle">CONTACTS</li>
+                                                                   <li class="footerNavStyle">Phone: 0504501678</li>
+                                                                   <li class="footerNavStyle" >Skype: BessarabEvgany</li>
+                                                                  </ul>
+                                                                 </div>
+                                                                    <div class="col-lg-3">
+                                                                     <p class="fallowUsStyle">FALLOW US</p>
+                                                                     <i class="fab fa-facebook fa-3x"></i>
+                                                                     <i class="fab fa-twitter-square fa-3x"></i>
+                                                                     <i class="fab fa-instagram fa-3x"></i>  
+                                                                     </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="FooterLineWrap">
+                                                            <div class="footerlineOne"></div>
+                                                            <div class="footerlineTwo"></div>
+                                                            <div class="footerlineThree"></div>
+                                                            <div class="footerlineFour"></div>
+                                                        </div>
+                                                    </div>
+                                              
+                                                
+
+                        
+    
+
+    
+    </body>
+
+</html>
+
